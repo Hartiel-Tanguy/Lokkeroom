6d4447c3197ae632aa1cc40af55a2e48e1fcdcc0
Antoine <tanguy.hartiel.1@gmail.com>
2022-06-21 16:11:15 +0200

    test456 pour redirect une page

--- public/app.js
+++ public/app.js
@@ -1,4 +1,16 @@
 //route pour aller sur la page d'accueil si nom et mot de passe sont bon
-fuction
+
+//redirect vers une autre page html si le nom et le mot de passe sont bon
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == "admin" && password == "admin") {
+        window.location.href = "index.html";
+    } else {
+        alert("Wrong username and/or password");
+    }
+}
+
+
 
 
